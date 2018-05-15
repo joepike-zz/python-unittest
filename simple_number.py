67bff5699114d73dd2c1ef8ce4123aa3069ef0bd
joe <joe@joes-MacBook-Pro.local>
2018-05-15 14:08:46 +0100

    added init function in SimpleNumber class

--- simple_number.py
+++ simple_number.py
@@ -1,10 +1,4 @@
 
 class SimpleNumber:
     def __init__(self, num):
-        self.x = num
-
-    def add(num):
-        self.x += num
-
-    def multiply(num):
-        self.x * num
+        self.val = num
--- test_simple_number.py
+++ test_simple_number.py
@@ -1,5 +1,5 @@
-from simple_number import SimpleNumber
 import unittest
+from simple_number import SimpleNumber
 
 class TestSimpleNumber(unittest.testCase):
 
@@ -10,3 +10,6 @@ class TestSimpleNumber(unittest.testCase):
     def test_add(self):
         num = SimpleNumber(2)
         self.assertEqual(num.add(2))
+
+if __name__ == "__main__":
+    unittest.main()
